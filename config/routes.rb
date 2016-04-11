3c6167362083829b9d931bfe3be2adc1e1dc63ef
Paul Christensen <paullchristenen@gmail.com>
2016-04-11 19:16:04 +0000

    Add edit udpate functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-   root 'pages#home'
-   get 'about', to: 'pages#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
