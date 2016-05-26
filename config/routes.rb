f872991a9b02821e539199c16d7ffea207a2d4ad
Paul Christensen <paullchristenen@gmail.com>
2016-05-26 18:31:40 +0000

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,14 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
-  
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
