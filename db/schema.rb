f385ea8de44dd50d32436c08f5c974550004042e
Paul Christensen <paullchristenen@gmail.com>
2016-06-08 01:34:23 +0000

    add article and category ass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605151627) do
+ActiveRecord::Schema.define(version: 20160608012203) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
